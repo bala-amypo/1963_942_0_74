3239a9570c1d4e8c012d137ea2bc2de26f77f0db
bala-amypo <bala@amypo.in>
2025-12-17 09:28:37 +0000

    User commit

--- src/main/java/com/example/demo/serviceimplement/Stuserviceimplement.java
+++ src/main/java/com/example/demo/serviceimplement/Stuserviceimplement.java
@@ -3,6 +3,6 @@ package com.example.demo.serviceimplement;
 import org.springframework.stereotype.Service;
 import com.example.demo.service.Stuservice;
 @Service
-public class Stuserviceimplement implements  {
+public class Stuserviceimplement implements Stuservice{
 
 }
